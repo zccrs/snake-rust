f9231fd8da03edb7b2385e31e86934e0ec7028f3
zccrs <zccrs@live.com>
2021-01-28 22:28:10 +0800

    feat: add 'setCursorVisible' function
    add "ascii" crate for unix target.
    add initial codes for main function.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 extern crate ncursesw;
+extern crate ascii;
 
+use ascii::*;
 use ncursesw::*;
+use ncursesw::normal::*;
 
 fn moveCursor(x: i32, y: i32) {
-    r#move(Origin { y: x, x: y });
+    r#move(Origin { y: x, x: y }).unwrap();
+}
+
+fn setCursorVisiable(visible: bool) {
+    curs_set(if visible {CursorType::Visible} else {CursorType::Invisible}).unwrap();
 }
 
 fn main() {
+    initscr().unwrap();
 
+    if has_colors() {
+        start_color();
+        use_default_colors();
+
+    } else {
+        addstr("terminal has no color support!!!");
+    }
+
+    endwin();
 }
